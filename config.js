3c639a30f0f35426be0fbfb21058e31da33616da
mtaju <152062507+mtaju@users.noreply.github.com>
2024-02-03 14:20:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.Owner = ["+255744181216", "+255734980103"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+255734980103", "+255744181216"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+255734980103"];
+global.OwnerName = "Mr Mtaju";
+global.BotName = "ꪶꪖɀꪖᥴ𝘬-ꪑᦔ";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
